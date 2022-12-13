28f50a4372ce752e0e402fcaf9d20f534b16e166
Melisha14 <105646979+Melisha14@users.noreply.github.com>
2022-12-13 16:14:46 +0100

    valider

--- StringCalculator.test/UnitTest1.cs
+++ StringCalculator.test/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace StringCalculator.test
         [Theory(DisplayName = "ETANT DONNE une chaine \"x,y\" " +
                               "QUAND on appelle Add " +
                               "ALORS on a x + y")]
-        [InlineData(1,2)]
+        [InlineData(1, 2)]
         public void TestAjouter(int x, int y)
         {
             // ETANT une chaîne "1, 2"
@@ -15,17 +15,17 @@ namespace StringCalculator.test
             // QUAND on appelle add
             var result = AddString.Add(debut);
             // ALORS on a x+y
-            Assert.Equal(x+y, result);
+            Assert.Equal(x + y, result);
         }
 
 
-
+/*
         [Theory(DisplayName = "ETANT DONNE une chaine \"x,y,z\" " +
                               "QUAND on appelle Add " +
                               "ALORS on a somme de plusieurs nombres")]
 
         [InlineData(1, 2, 3)]
-        public void TestPlusDeDeuxNombres(int x, int y, int z)
+      ""  public void TestPlusDeDeuxNombres(int x, int y, int z)
         {
             // ETANT n nombres "1, 2, 3"
             var debut = $"{x}, {y}, {z}";
@@ -33,16 +33,25 @@ namespace StringCalculator.test
             var result = AddString.Add(debut);
             // ALORS on a somme de plusieurs nombres
             Assert.Equal(x + y + z, result);
+        }*/
+
+
+        [Fact]
+        public void TestSaut_de_Lignes()
+        {
+
+            // ETANT DONNE une liste de nombres de la forme "1,2,..."comportant un saut de ligne
+            var debut = string.Join(',', new int[] { 1, 2, 3 });
+            var entréeAvecSautDeLigne = AddString.Add(debut);
+
+            // QUAND on appelle Add
+            var resultAvecSautDeLigne = AddString.Add(debut);
+
+            // ALORS le résultat est le même que pour un debut n'en ayant pas
+
+            var resultSansSautDeLigne = AddString.Add(debut);
+
+            Assert.Equal(resultSansSautDeLigne, resultAvecSautDeLigne);
         }
-        /*
-                [Theory(DisplayName = "ETANT DONNE une chaine \"x,y\" " contenant un saut_de_ligne+
-                                      "QUAND on appelle Add " +
-                                      "ALORS on obtient x + y")]
-                [InlineData()]
-                public void TestSaut_de_Lignes()
-                {
-
-                }*/
     }
-
 }
